db01cb8b063592dee168688b4f534f92a078bea3
Marius Stoica <stoicamarius9010@gmail.com>
2024-11-04 23:06:39 +0100

    Add GameGridBackgroundColor in Color.kt

--- app/src/main/java/com/katas/tictactoe/ui/theme/Color.kt
+++ app/src/main/java/com/katas/tictactoe/ui/theme/Color.kt
@@ -9,3 +9,4 @@ val Pink40 = Color(0xFF7D5260)
 val CrossColor = Color(0xFFE32F17)
 val CircleColor = Color.White
 val GameSquareBackgroundColor = Color(0xF221865F)
+val GameGridBackgroundColor = Color(0xFF1C6C4D)
