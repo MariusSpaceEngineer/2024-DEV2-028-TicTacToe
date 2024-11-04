5a587d12f5fc0118fa45502d30c0569464af567a
Marius Stoica <stoicamarius9010@gmail.com>
2024-11-04 23:08:15 +0100

    Add GameGrid to MainActivity

--- app/src/main/java/com/katas/tictactoe/MainActivity.kt
+++ app/src/main/java/com/katas/tictactoe/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.katas.tictactoe.composables.GameSquare
+import com.katas.tictactoe.composables.GameGrid
 import com.katas.tictactoe.ui.theme.TicTacToeTheme
 import com.katas.tictactoe.utils.SquareState
 
@@ -14,7 +14,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TicTacToeTheme {
-              GameSquare(state = SquareState.None, onClick = {})
+                val boardState = List(3) { List(3) { SquareState.None } }
+                GameGrid(boardState = boardState, onSquareClick = { _, _ ->
+                    // Handle square click
+                })
             }
         }
     }
