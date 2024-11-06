c09dc6df58532b9a09f93c946e3858df57bff21d
Marius Stoica <stoicamarius9010@gmail.com>
2024-11-06 22:47:18 +0100

    Create GameScore composable

---
+++ app/src/main/java/com/katas/tictactoe/composables/GameScore.kt
@@ -0,0 +1,22 @@
+package com.katas.tictactoe.composables
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * Composable function that displays the current score of the game session.
+ */
+@Composable
+fun GameScore(player1Score: Int, player2Score: Int) {
+    Text(
+        text = "$player1Score - $player2Score",
+        modifier = Modifier.padding(16.dp),
+        fontSize = 64.sp,
+        color = Color.Black
+    )
+}
